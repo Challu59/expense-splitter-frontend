8babff34a8d6d35d914b916e4bc84419e9a2b704
Challu59 <aayushadhikari72@gmail.com>
2025-12-19 21:45:58 +0545

    add create group functionality in groups_screen.dart

--- lib/screens/groups_screen.dart
+++ lib/screens/groups_screen.dart
@@ -10,6 +10,60 @@ class _GroupsScreenState extends State<GroupsScreen>{
   List groups = [];
   bool loading = true;
 
+  void showCreateGroupSheet() {
+    final nameController = TextEditingController();
+    final currencyController = TextEditingController(text: "NPR");
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (_) {
+        return Padding(
+          padding: EdgeInsets.fromLTRB(
+            16,
+            16,
+            16,
+            MediaQuery.of(context).viewInsets.bottom + 16,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "Create Group",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 12),
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: "Group Name"),
+              ),
+              TextField(
+                controller: currencyController,
+                decoration: const InputDecoration(labelText: "Currency"),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () async {
+                  final success = await ApiService.createGroup(
+                    nameController.text,
+                    currencyController.text,
+                  );
+
+                  if (success) {
+                    Navigator.pop(context);
+                    fetchGroups(); // refresh list
+                  }
+                },
+                child: const Text("Create"),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+
   @override
   void initState() {
     super.initState();
@@ -31,11 +85,7 @@ class _GroupsScreenState extends State<GroupsScreen>{
       appBar: AppBar(title: Text("Your Groups"),),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: (){
-
-          // Add group ui
-
-        },
+        onPressed: showCreateGroupSheet
       ),
       body: loading?
       const Center(
@@ -48,7 +98,7 @@ class _GroupsScreenState extends State<GroupsScreen>{
               child: ListTile(
                 title: Text(group["name"]),
                 subtitle: Text(
-                  "${group[members_count]} members"
+                  "${group['members_count']} members"
                 ),
                 onTap: (){
                   // group details ui
